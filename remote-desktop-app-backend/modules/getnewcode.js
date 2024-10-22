41cec4da751ae4d630fe965d4da1d3902bf14508
Gabriel Ekodo <ekodogabriel@gmail.com>
2024-10-22 10:11:37 +0300

    code generator updated

--- remote-desktop-app-backend/modules/getnewcode.js
+++ remote-desktop-app-backend/modules/getnewcode.js
@@ -5,8 +5,14 @@ const { authenticateToken } = require('./auth')
 module.exports = (app) => {
   // get new connection code
   app.get('/getcode', authenticateToken, (req, res) => {
-    const code = randomize('A0', 8)
+    const code = randomize('A0', 12)
     randomize.isCrypto
     res.json({ code })
+  })
+
+  // add pin
+  app.post('/pincode', authenticateToken, (req, res) => {
+    req.body
+    res.json({ pin: req.body })
   })
 }
